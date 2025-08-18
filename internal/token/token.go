89069d63705f9bf1873265cef16a9847ffd33f14
LxrdShadow <andritiana.idealy@gmail.com>
2025-08-18 22:05:30 +0300

    feat: add String method to token for easier debug

--- internal/token/token.go
+++ internal/token/token.go
@@ -17,3 +17,24 @@ type Token struct {
 	Value string // The text content
 	Level int    // Used only for headers (#=1, ##=2, ...)
 }
+
+func (t Token) String() string {
+	switch t.Type {
+	case TokenHeader:
+		return "HEADER(" + string(rune('0'+t.Level)) + ", " + t.Value + ")"
+	case TokenParagraph:
+		return "PARAGRAPH(" + t.Value + ")"
+	case TokenText:
+		return "TEXT(" + t.Value + ")"
+	case TokenBold:
+		return "BOLD(" + t.Value + ")"
+	case TokenItalic:
+		return "ITALIC(" + t.Value + ")"
+	case TokenCodeSpan:
+		return "CODESPAN(" + t.Value + ")"
+	case TokenEOF:
+		return "EOF"
+	}
+
+	return "UNKNOWN(" + t.Value + ")"
+}
