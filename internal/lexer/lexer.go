95ffd122c88d897aef274f5042d00d0665f465fe
LxrdShadow <andritiana.idealy@gmail.com>
2025-08-20 23:07:11 +0300

    feat(lexer): add inline lexing for bold texts

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -3,6 +3,9 @@ package lexer
 import (
 	"bufio"
 	"bytes"
+	"fmt"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/LxrdShadow/live.md/internal/token"
 )
@@ -29,6 +32,7 @@ func (l *Lexer) Lex() []token.Token {
 }
 
 func (l *Lexer) lexLine(line string) token.Token {
+	fmt.Println("lex line:", len(line))
 	var tok token.Token
 
 	if len(line) > 0 && line[0] == '#' {
@@ -52,8 +56,62 @@ func (l *Lexer) lexHeader(line string) token.Token {
 	if pos < len(line) && line[pos] == ' ' {
 		pos++ // Skip the space
 
-		return token.Token{Type: token.TokenHeader, Value: line[pos:], Level: min(level, 6)}
+		return token.Token{Type: token.TokenHeader, Children: l.lexInline(line[pos:]), Level: min(level, 6)}
 	} else {
 		return token.Token{Type: token.TokenText, Value: line}
 	}
+}
+
+func (l *Lexer) lexInline(line string) []token.Token {
+	tokens := []token.Token{}
+	buf := []rune{}
+
+	flushBuf := func(tt token.TokenType) {
+		if len(buf) > 0 {
+			tokens = append(tokens, token.Token{
+				Type:  tt,
+				Value: string(buf),
+			})
+			buf = []rune{}
+		}
+	}
+
+	for i := 0; i < len(line); {
+		r, size := utf8.DecodeRuneInString(line[i:])
+
+		switch r {
+		// Handle bold text
+		case '*':
+			if i+1 < len(line) && line[i+1] == '*' {
+				flushBuf(token.TokenText)
+				end := l.findClosing(line, i+2, "**")
+
+				if end != -1 {
+					tok := token.Token{
+						Type:     token.TokenBold,
+						Children: l.lexInline(line[i+2 : end]),
+					}
+					tokens = append(tokens, tok)
+					i = end + len("**")
+				} else {
+					buf = append(buf, '*', '*')
+					i += 2
+				}
+			}
+		default:
+			buf = append(buf, r)
+			i += size
+		}
+	}
+
+	flushBuf(token.TokenText)
+	return tokens
+}
+
+func (l *Lexer) findClosing(s string, start int, delim string) int {
+	idx := strings.Index(s[start:], delim)
+	if idx == -1 {
+		return idx
+	}
+	return start + idx
 }
