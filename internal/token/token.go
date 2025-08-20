a1e4b17d78764c2e639033a296fab51a75f0c640
LxrdShadow <andritiana.idealy@gmail.com>
2025-08-20 21:31:51 +0300

    feat(token): add children field (mostly for Headers and Paragraphs)

--- internal/token/token.go
+++ internal/token/token.go
@@ -13,9 +13,10 @@ const (
 )
 
 type Token struct {
-	Type  TokenType
-	Value string // The text content
-	Level int    // Used only for headers (#=1, ##=2, ...)
+	Type     TokenType
+	Value    string // The text content
+	Children []Token
+	Level    int // Used only for headers (#=1, ##=2, ...)
 }
 
 func (t Token) String() string {
