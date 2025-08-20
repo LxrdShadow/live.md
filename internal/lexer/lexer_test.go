0fe3f27de815f4bff8784605c4d0b23008495085
LxrdShadow <andritiana.idealy@gmail.com>
2025-08-20 23:07:32 +0300

    test(lexer): add test for headers

---
+++ internal/lexer/lexer_test.go
@@ -0,0 +1,30 @@
+package lexer
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/LxrdShadow/live.md/internal/token"
+)
+
+func TestLexingHeader(t *testing.T) {
+	input := "# Hello world"
+	l := New(input)
+	tokens := l.Lex()
+
+	if len(tokens) != 1 {
+		t.Fatalf("len(tokens) is not %d. got=%d", 1, len(tokens))
+	}
+
+	if tokens[0].Type != token.TokenHeader {
+		t.Fatalf("token is not HEADER. got=%s", tokens[0])
+	}
+
+	if tokens[0].Children[0].Type != token.TokenText {
+		t.Fatalf("token child is not TEXT. got=%s", tokens[0].Children[0])
+	}
+
+	if tokens[0].Children[0].Value != "Hello world" {
+		t.Fatalf("value of token child is not \"Hello world\". got=%s", tokens[0].Children[0].Value)
+	}
+}
