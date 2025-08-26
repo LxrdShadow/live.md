e2f755d26bc098b93eea76b858d3805a88e321e3
LxrdShadow <andritiana.idealy@gmail.com>
2025-08-26 20:54:26 +0300

    feat(parser): add a parser constructor function

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -3,5 +3,9 @@ package parser
 import "github.com/LxrdShadow/live.md/internal/token"
 
 type Parser struct {
-	tokens token.Token
+	tokens []token.Token
+}
+
+func New(tokens []token.Token) *Parser {
+	return &Parser{tokens: tokens}
 }
