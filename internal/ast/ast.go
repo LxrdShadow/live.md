28e04bc642dbc6812d8d4f49df28755d21c4dae4
LxrdShadow <andritiana.idealy@gmail.com>
2025-08-29 23:47:09 +0300

    feat(ast): add String() method for debug purposes

--- internal/ast/ast.go
+++ internal/ast/ast.go
@@ -1,5 +1,10 @@
 package ast
 
+import (
+	"fmt"
+	"strings"
+)
+
 const (
 	DOCUMENT  = "DOCUMENT"
 	HEADER    = "HEADER"
@@ -14,4 +19,21 @@ type Node struct {
 	Value    string
 	Children []Node
 	Level    int
+}
+
+func (n Node) String() string {
+	if len(n.Children) > 0 {
+		childrenStr := []string{}
+
+		for _, child := range n.Children {
+			childrenStr = append(childrenStr, child.String())
+		}
+		return fmt.Sprintf("%s[%s]", n.Type, strings.Join(childrenStr, ", "))
+	}
+
+	if n.Value != "" {
+		return fmt.Sprintf("%s(\"%s\")", n.Type, n.Value)
+	}
+
+	return n.Type
 }
