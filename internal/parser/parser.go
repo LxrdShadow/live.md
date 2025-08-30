b9f55a8c37713d0e27f66e37c42632181162e486
LxrdShadow <andritiana.idealy@gmail.com>
2025-08-30 21:47:07 +0300

    feat(parser): add parsing of default inline blocks

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -45,10 +45,10 @@ func (p *Parser) parseBlock() *ast.Node {
 		for range level {
 			p.consume()
 		}
-		return &ast.Node{Type: ast.HEADER, Level: level}
+		return &ast.Node{Type: ast.HEADER, Level: level, Children: p.parseInlineUntil(token.NEWLINE)}
 	default:
 		p.consume()
-		return &ast.Node{Type: ast.PARAGRAPH}
+		return &ast.Node{Type: ast.PARAGRAPH, Children: p.parseInlineUntil(token.NEWLINE)}
 	}
 }
 
@@ -60,4 +60,81 @@ func (p *Parser) getHeaderLevel(start int) int {
 	}
 
 	return pos
+}
+
+func (p *Parser) findClosing(t token.TokenType) int {
+	for pos := p.pos; pos < len(p.tokens); pos++ {
+		if p.tokens[pos].Type == token.NEWLINE {
+			return -1
+		}
+
+		if p.tokens[pos].Type == t {
+			return pos
+		}
+	}
+
+	return -1
+}
+
+func (p *Parser) parseInlineUntil(stop token.TokenType) []*ast.Node {
+	nodes := []*ast.Node{}
+
+	for p.current().Type != stop && p.current().Type != token.EOF {
+		tok := p.consume()
+		switch tok.Type {
+		case token.BOLD:
+			end := p.findClosing(token.BOLD)
+
+			if end != -1 {
+				nodes = append(nodes, &ast.Node{
+					Type:     ast.BOLD,
+					Children: p.parseInlineUntil(token.BOLD),
+				})
+			} else {
+				nodes = append(nodes, &ast.Node{
+					Type:  ast.TEXT,
+					Value: tok.Value,
+				})
+			}
+		case token.ITALIC:
+			end := p.findClosing(token.ITALIC)
+
+			if end != -1 {
+				nodes = append(nodes, &ast.Node{
+					Type:     ast.ITALIC,
+					Children: p.parseInlineUntil(token.ITALIC),
+				})
+			} else {
+				nodes = append(nodes, &ast.Node{
+					Type:  ast.TEXT,
+					Value: tok.Value,
+				})
+			}
+		case token.BOLDITALIC:
+			end := p.findClosing(token.BOLDITALIC)
+
+			if end != -1 {
+				nodes = append(nodes, &ast.Node{
+					Type:     ast.BOLDITALIC,
+					Children: p.parseInlineUntil(token.BOLDITALIC),
+				})
+			} else {
+				nodes = append(nodes, &ast.Node{
+					Type:  ast.TEXT,
+					Value: tok.Value,
+				})
+			}
+		default:
+			nodes = append(nodes, &ast.Node{
+				Type:  ast.TEXT,
+				Value: tok.Value,
+			})
+		}
+	}
+
+	if p.current().Type == stop {
+		p.consume()
+	}
+
+	return nodes
 }
