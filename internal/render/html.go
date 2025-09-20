dbfd795b0b64d586798945e89b876a830bfb6061
LxrdShadow <andritiana.idealy@gmail.com>
2025-09-20 20:05:23 +0300

    feat(render): add renderNode() method to render nodes depending on their type

--- internal/render/html.go
+++ internal/render/html.go
@@ -1,7 +1,58 @@
 package render
 
+import (
+	"strings"
+
+	"github.com/LxrdShadow/live.md/internal/ast"
+)
+
 type HTMLRenderer struct{}
 
 func NewHTMLRenderer() *HTMLRenderer {
 	return &HTMLRenderer{}
+}
+
+func (r *HTMLRenderer) renderNode(sb *strings.Builder, n *ast.Node) {
+	switch n.Type {
+	case ast.DOCUMENT:
+		for _, c := range n.Children {
+			r.renderNode(sb, c)
+		}
+	case ast.PARAGRAPH:
+		sb.WriteString("<p>")
+		for _, c := range n.Children {
+			r.renderNode(sb, c)
+		}
+		sb.WriteString("</p>\n")
+	case ast.HEADER:
+		sb.WriteString("<h")
+		sb.WriteString(string(rune('0' + n.Level))) // convert int to char
+		sb.WriteString(">")
+
+		for _, c := range n.Children {
+			r.renderNode(sb, c)
+		}
+
+		sb.WriteString("</h")
+		sb.WriteString(string(rune('0' + n.Level))) // convert int to char
+		sb.WriteString(">\n")
+	case ast.BOLD:
+		sb.WriteString("<strong>")
+		for _, c := range n.Children {
+			r.renderNode(sb, c)
+		}
+		sb.WriteString("</strong>")
+	case ast.ITALIC:
+		sb.WriteString("<em>")
+		for _, c := range n.Children {
+			r.renderNode(sb, c)
+		}
+		sb.WriteString("</em>")
+	case ast.CODESPAN:
+		sb.WriteString("<code>")
+		sb.WriteString(n.Value)
+		sb.WriteString("</code>")
+	case ast.TEXT:
+		sb.WriteString(n.Value)
+	}
 }
